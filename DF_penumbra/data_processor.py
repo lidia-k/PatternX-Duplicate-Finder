a42108d5cd54a21028c428826eb1abaf37df256a
Lidia Kwon <lidiakwon212@gmail.com>
2024-03-15 17:06:02 -0700

    add the logging

--- DF_penumbra/data_processor.py
+++ DF_penumbra/data_processor.py
@@ -160,6 +160,7 @@ class DataProcessor:
                 RETURN n.npi, n.id, n.fname, n.lname, n.fullname
                 '''
             results = session.run(query).data()
+
             for result in results:
                 id = result['n.id']
                 npi = result['n.npi']
@@ -179,15 +180,24 @@ class DataProcessor:
                     if len(reg_r) > 1:
                         print(f"More than one NPI returned for {npi}, {id}")
                     else: 
-                        basic = reg_r[0]['basic']
+                        try: 
+                            basic = reg_r[0]['basic']
+                        except IndexError:
+                            print(f"No data returned for NPI {npi}, {id}: {reg_r}")
+                            continue
                         fname = basic.get('first_name', None)
                         lname = basic.get('last_name', None)
                         if not fname and not lname:
                             print(f"Missing name for NPI {npi}, {id} in the registry")
                             continue
+                        
+                        output_f = open('npi_val.txt', 'w')
                         name = f"{fname} {lname}"
                         ratio = fuzz.ratio(fullname.lower(), name.lower())
-                        print(name, fullname, ratio)
+                        if ratio != 100:
+                            output_f.write(f'{fullname} vs {name} : {ratio} for NPI {npi}, {id}\n')
+                        
+                        output_f.close()
                 else:
                     print(f"Failed to retrieve data for NPI {npi}, {id}: {res.status_code}")
                 
