90b1de2e385fc6d6ac3b82cd056dcb46cbc1e30d
Lidia Kwon <lidiakwon212@gmail.com>
2024-03-20 15:44:10 -0700

    update the directed edge to undirected

--- src/DF_penumbra/duplicate_finder.py
+++ src/DF_penumbra/duplicate_finder.py
@@ -16,46 +16,34 @@ class DuplicateFinder:
             name_q = '''
                     MATCH (a),(b)
                     WHERE a.fullname = b.fullname AND id(a) < id(b)
-                    MERGE (a)-[:r1_fullname]->(b)
+                    MERGE (a)-[:r1_fullname]-(b)
                     RETURN count(*)
                     '''
             name_npi_q = '''
                     MATCH (a),(b)
                     WHERE a.fullname = b.fullname AND a.npi = b.npi AND id(a) < id(b)
-                    MERGE (a)-[:r1_fullname_npi]->(b)
+                    MERGE (a)-[:r1_fullname_npi]-(b)
                     RETURN count(*)
                     '''
             name_country_q = '''
                     MATCH (a),(b)
                     WHERE a.fullname = b.fullname AND a.country = b.country AND id(a) < id(b)
-                    MERGE (a)-[:r1_fullname_country]->(b)
+                    MERGE (a)-[:r1_fullname_country]-(b)
                     RETURN count(*)
                     '''
             name_s_q = '''
                     MATCH (a),(b)
                     WHERE a.fullname = b.fullname AND a.specialty = b.specialty AND id(a) < id(b)
-                    MERGE (a)-[:r1_fullname_speciality]->(b)
-                    RETURN count(*)
-                    '''
-            name_sno_q = '''
-                    MATCH (a),(b)
-                    WHERE a.fullname = b.fullname AND a.sap_no = b.sap_no AND id(a) < id(b)
-                    MERGE (a)-[:r1_fullname_sap]->(b)
+                    MERGE (a)-[:r1_fullname_speciality]-(b)
                     RETURN count(*)
                     '''
             name_email_q = '''
                     MATCH (a),(b)
                     WHERE a.fullname = b.fullname AND a.email = b.email AND id(a) < id(b)
-                    MERGE (a)-[:r1_fullname_email]->(b)
+                    MERGE (a)-[:r1_fullname_email]-(b)
                     RETURN count(*)
                     '''
-            name_qb_q = '''
-                    MATCH (a),(b)
-                    WHERE a.fullname = b.fullname AND a.qb_id = b.qb_id AND id(a) < id(b)
-                    MERGE (a)-[:r1_fullname_qb]->(b)
-                    RETURN count(*)
-                    '''
-            queries = [name_q, name_npi_q, name_country_q, name_s_q, name_sno_q, name_email_q, name_qb_q]
+            queries = [name_q, name_npi_q, name_country_q, name_s_q, name_email_q]
             for query in queries:
                 result = session.run(query)
                 print(f'Found {result.single()[0]} duplicates')
