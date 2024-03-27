816b04e55b0288ce72d08dfe54155fff02570bed
Lidia Kwon <lidiakwon212@gmail.com>
2024-03-26 20:59:18 -0700

    similarity_search wip

--- src/DF_penumbra/duplicate_finder.py
+++ src/DF_penumbra/duplicate_finder.py
@@ -1,3 +1,6 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Neo4jVector
+
 from dao.NEO4J_Graph import Graph
 from utils import auto_config as config
 
@@ -18,6 +21,18 @@ class DuplicateFinder:
             config.NEO4J_USER,
             config.NEO4J_PASSWORD
         )
+        self.embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+        for node in ['Provider', 'Speaker']:
+            self.vector_graph = Neo4jVector.from_existing_graph(
+                embedding=self.embeddings,
+                url=config.NEO4J_URL,
+                username=config.NEO4J_USER,
+                password=config.NEO4J_PASSWORD,
+                index_name='penumbra_index',
+                node_label=node,
+                text_node_properties=['text'],
+                embedding_node_property='embedding'
+            )
 
     def build_duplicate_edges(self):
         driver = self.graph.get_driver()
@@ -52,3 +67,25 @@ class DuplicateFinder:
                     '''
                 result = session.run(q).data()
                 print(f'Found {len(result)} duplicates for {type}')
+    
+    def similarity_search(self):
+        driver = self.graph.get_driver()
+        with driver.session() as session:
+            for node_type in ['Provider', 'Speaker']:
+                q = f'''
+                    MATCH (n:{node_type})
+                    RETURN n
+                    '''
+                result = session.run(q).data()
+                print(f'Found {len(result)} nodes for {node_type}')
+                for node in result:
+                    node = node['n']
+                    q = node['text']
+                    print(node['fullname'])
+
+                    results = self.vector_graph.similarity_search_with_score(q)
+                    for result in results:
+                        doc, score = result
+                        fullname = doc.metedata.get('fullname')
+                        npi = doc.metadata.get('npi')
+                        print(score, fullname, npi)
