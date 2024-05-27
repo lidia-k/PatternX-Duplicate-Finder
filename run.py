212eca50196a36f7168ebd03e78e8f2e55d9f9b1
Lidia Kwon <lidiakwon212@gmail.com>
2024-05-27 13:18:59 +0900

    remove the debugger

--- run.py
+++ run.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
         print('Preparing training data with{} NPI...'.format('' if args.npi else 'out'))
         dp = DataPreprocessor(data_dir, include_npi=args.npi, training=True)
         ltable, rtable, data = dp.prepare_training_data(skewed_factor=2, size=args.size, model=args.m_model)
-        import pdb; pdb.set_trace()
+
         print('Training {} with{} NPI...'.format(args.m_model, '' if args.npi else 'out'))
         mt = MagellanTrainer(ltable, rtable, data, model=args.m_model, training=True)
         mt.train_model()
