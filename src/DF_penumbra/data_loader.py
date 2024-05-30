a60c300dae8ef369cf32004cbf46f04d3302a4a0
Lidia Kwon <lidiakwon212@gmail.com>
2024-05-30 19:01:18 +0900

    fix the function to return one result

--- src/DF_penumbra/data_loader.py
+++ src/DF_penumbra/data_loader.py
@@ -94,7 +94,7 @@ class Neo4jDataLoader:
 
         df.to_csv(f'./{file_name}', index=False)
         print(f'Updated file: {file_name}')
-        return file_name, df
+        return file_name
 
     def _load_data_from_cypher(self, file_path):
         if 'sp' in file_path:
@@ -133,7 +133,6 @@ class Neo4jDataLoader:
 
         # Update csv files and load data to Neo4j
         data_bundles = glob.glob(f'{self.data_dir}/*.csv')
-        df_dict = {}
         for f in data_bundles:
             fname = self._prepare_csv_file(f)
             self._load_data_from_cypher(fname)
