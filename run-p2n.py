21f0774c81f2d4bf6b2acf22f533503f6235b3d8
win <khacnha.it@gmail.com>
2024-08-12 17:51:49 +0700

    fix: size

--- run-p2n.py
+++ run-p2n.py
@@ -216,9 +216,9 @@ elif args.project == 'penumbra' and args.task == 'train-ditto':    #sn  added el
     runtag    = '%s_lm=%s_da=%s_dok=%s_su=%s_clipdaet=%s_id=%d' % ( args.task, args.lm, 
         args.da, args.dok, args.summarize, str(args.clipdaet), args.run_id )
     runtag    = runtag.replace('/', '_')
-    traindaet = dida.DittoDataset( trainpath, lm=args.lm, max_len=args.max_len, clipdaet=args.clipdaet, da=args.da )
-    validdaet = dida.DittoDataset( validpath, lm=args.lm, clipdaet=args.clipdaet )   #sn validdaet.pairs[1] is plain text
-    testdaet  = dida.DittoDataset( testpath , lm=args.lm, clipdaet=args.clipdaet )
+    traindaet = dida.DittoDataset( trainpath, lm=args.lm, max_len=args.max_len, size=args.clipdaet, da=args.da )
+    validdaet = dida.DittoDataset( validpath, lm=args.lm, size=args.clipdaet )   #sn validdaet.pairs[1] is plain text
+    testdaet  = dida.DittoDataset( testpath , lm=args.lm, size=args.clipdaet )
     start_time = time.strftime("%Y%m%d-%H%M%S")
     print( "train start = "  + start_time )
     didi.train( traindaet, validdaet, testdaet, runtag, args )
